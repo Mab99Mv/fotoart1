39827ea70cbc8fee5544d0063151f3e84f347847
Mab99V <mabelperezgaribay@gmail.com>
2023-06-05 04:17:40 -0500

    visualizar reacciones y comen

--- src/components/Reacciones.component.js
+++ src/components/Reacciones.component.js
@@ -1,5 +1,4 @@
 import '../reacciones.css';
-import KafkaService from '../services/kafka.service';
 import React, { Component } from 'react';
 import axios from 'axios';
 
@@ -8,18 +7,33 @@ class Reacciones extends Component {
     super(props);
     this.state = {
       reactions: {},
+      totalReactions: 0,
     };
     this.uri = 'https://apimo-service-new-mab99mv.cloud.okteto.net'; // Actualiza la URL base aquí
   }
 
   componentDidMount() {
+    this.fetchTotalReactions();
     this.fetchReactions();
   }
+
+  fetchTotalReactions = () => {
+    axios
+      .get(`${this.uri}/api/reactions`)
+      .then((response) => {
+        const totalReactions = response.data.reactions.length;
+        this.setState({ totalReactions });
+      })
+      .catch((error) => {
+        console.log('Error al obtener el número total de reacciones:', error);
+      });
+  };
+
   fetchReactions = () => {
     const { id } = this.props;
-  
+
     axios
-      .get(`${this.uri}/reactions/${encodeURIComponent(id)}/count`)
+      .get(`${this.uri}/api/reactions/${encodeURIComponent(id)}/count`)
       .then((response) => {
         const { count } = response.data;
         this.setState((prevState) => ({
@@ -33,7 +47,7 @@ class Reacciones extends Component {
         console.log('Error al obtener las reacciones:', error);
       });
   };
-  
+
   sendReaction = (status) => {
     const user = localStorage.getItem('user');
     const data = {
@@ -43,7 +57,7 @@ class Reacciones extends Component {
     };
     console.log('Datos de reacción:', data);
     console.log(JSON.stringify(data));
-    KafkaService.reactionPush(data);
+    // Aquí puedes realizar la lógica para enviar la reacción a la API
   };
 
   reaction = (e, status) => {
@@ -54,57 +68,45 @@ class Reacciones extends Component {
 
   render() {
     const { id } = this.props;
-    const { reactions } = this.state;
+    const { reactions, totalReactions } = this.state;
     const reactionCount = reactions[id] || 0;
 
     return (
       <div className="reactions">
-        <div
-          className="reaction reaction-like"
-          onClick={(e) => this.reaction(e, 'like')}
-        >
+        <div className="reaction reaction-like" onClick={(e) => this.reaction(e, 'like')}>
           <tool-tip></tool-tip>
           <p>{reactionCount}</p>
+          <p>Total: {totalReactions}</p>
         </div>
 
-        <div
-          className="reaction reaction-love"
-          onClick={(e) => this.reaction(e, 'love')}
-        >
+        <div className="reaction reaction-love" onClick={(e) => this.reaction(e, 'love')}>
           <tool-tip></tool-tip>
           <p>{reactionCount}</p>
+          <p>Total: {totalReactions}</p>
         </div>
 
-        <div
-          className="reaction reaction-haha"
-          onClick={(e) => this.reaction(e, 'haha')}
-        >
+        <div className="reaction reaction-haha" onClick={(e) => this.reaction(e, 'haha')}>
           <tool-tip></tool-tip>
           <p>{reactionCount}</p>
+          <p>Total: {totalReactions}</p>
         </div>
 
-        <div
-          className="reaction reaction-wow"
-          onClick={(e) => this.reaction(e, 'wow')}
-        >
+        <div className="reaction reaction-wow" onClick={(e) => this.reaction(e, 'wow')}>
           <tool-tip></tool-tip>
           <p>{reactionCount}</p>
+          <p>Total: {totalReactions}</p>
         </div>
 
-        <div
-          className="reaction reaction-sad"
-          onClick={(e) => this.reaction(e, 'sad')}
-        >
+        <div className="reaction reaction-sad" onClick={(e) => this.reaction(e, 'sad')}>
           <tool-tip></tool-tip>
           <p>{reactionCount}</p>
+          <p>Total: {totalReactions}</p>
         </div>
 
-        <div
-          className="reaction reaction-angry"
-          onClick={(e) => this.reaction(e, 'angry')}
-        >
+        <div className="reaction reaction-angry" onClick={(e) => this.reaction(e, 'angry')}>
           <tool-tip></tool-tip>
           <p>{reactionCount}</p>
+          <p>Total: {totalReactions}</p>
         </div>
       </div>
     );
